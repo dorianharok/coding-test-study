30cc1b95380805a50daa17cfe1232fdd8de1978c
GHeeJeon <47032054+GHeeJeon@users.noreply.github.com>
2023-12-12 02:43:53 +0900

    Create 133502_전지희.java

---
+++ 1주차/133502_화/133502_전지희.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < ingredient.length; i++) {
+            stack.push(ingredient[i]);
+
+            // 스택의 크기가 4 이상이고, 맨 위가 빵, 그 다음이 고기, 그 다음이 야채, 그 아래가 빵일 경우
+            if (stack.size() >= 4 && stack.peek() == 1 && stack.get(stack.size() - 2) == 3 && stack.get(stack.size() - 3) == 2 && stack.get(stack.size() - 4) == 1) {
+                answer++;
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.pop();
+            }
+        }
+        return answer;
+    }
+}
