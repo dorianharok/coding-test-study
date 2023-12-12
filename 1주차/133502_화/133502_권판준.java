d3bae2bf18458554ba8ff57d62820bbe93a8e668
Panjun <33755218+Panjun-Kwon@users.noreply.github.com>
2023-12-12 23:50:25 +0900

    Update 133502_권판준.java

--- 1주차/133502_화/133502_권판준.java
+++ 1주차/133502_화/133502_권판준.java
@@ -16,6 +16,22 @@ public class No_133502 {
 
         return answer;
     }
-}
 
-// https://programmer-may.tistory.com/154
+    // https://programmer-may.tistory.com/154 풀이 참조
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        String bugger = "1231";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : ingredient) {
+            sb.append(i);
+            
+            if(sb.length() > 3 && sb.substring(sb.length() - 4, sb.length()).equals(bugger)) {
+                sb.delete(sb.length() - 4, sb.length());
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
