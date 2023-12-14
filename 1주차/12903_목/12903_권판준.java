1161dd449d16fbc896db01919244213e05d72b6b
Panjun <33755218+Panjun-Kwon@users.noreply.github.com>
2023-12-14 19:37:21 +0900

    [권판준] 1주차 목요일 과제 제출

---
+++ 1주차/12903_목/12903_권판준.java
@@ -0,0 +1,14 @@
+public class No_12903 {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] arr = s.split("");
+        if(arr.length % 2 == 1) {
+            answer += arr[arr.length / 2];
+        } else {
+            answer += arr[arr.length / 2 - 1] + arr[arr.length / 2];
+        }
+
+        return answer;
+    }
+}
