9c901d37793f9574c740930bb35895b01b600f2f
Panjun <33755218+Panjun-Kwon@users.noreply.github.com>
2023-12-21 08:28:18 +0900

    [권판준] 2주차 목요일 과제 제출

---
+++ 2주차/목_250125/250125_권판준.java
@@ -0,0 +1,20 @@
+public class No_250125 {
+    public int solution(String[][] board, int h, int w) {
+        int count = 0;
+        int[] dh = {0, 1, -1, 0};
+        int[] dw = {1, 0 ,0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int h_check = h + dh[i];
+            int w_check = w + dw[i];
+
+            if (h_check >= 0 && h_check < board.length && w_check >= 0 && w_check < board.length) {
+                if(board[h][w].equals(board[h_check][w_check])) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
