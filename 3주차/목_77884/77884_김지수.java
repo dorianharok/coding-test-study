a916e5e859b0ac337e15de210b614a1cc9ac61ff
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2023-12-30 02:56:04 +0900

    12954_김지수

--- 3주차/목_77884/77884_김지수.java
+++ 3주차/목_77884/77884_김지수.java
@@ -24,7 +24,7 @@ class Solution {
     public int solution(int left, int right) {
         int answer = 0;
 
-        for (int i=left;i<=right;i++) {
+        for (int i = left; i <= right; i++) {
             if (i % Math.sqrt(i) == 0) {
                 answer -= i;
             }
