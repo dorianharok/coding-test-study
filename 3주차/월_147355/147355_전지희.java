2a0c11c12feff5320440b4eba23002918cd2a589
GHeeJeon <47032054+GHeeJeon@users.noreply.github.com>
2024-01-01 15:41:21 +0900

    Update 147355_전지희.java

--- 3주차/월_147355/147355_전지희.java
+++ 3주차/월_147355/147355_전지희.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(String t, String p) {
         int count = 0;
-        int pValue = Integer.parseInt(p);
 
         for (int i = 0; i <= t.length() - p.length(); i++) {
             String substring = t.substring(i, i + p.length());
-            int substringValue = Integer.parseInt(substring);
 
-            if (substringValue <= pValue) {
+            if (substring.compareTo(p) <= 0) {
                 count++;
             }
         }
