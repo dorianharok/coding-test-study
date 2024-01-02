05c1d76147a45a68e6fdbaafd2b6b9ed4e093675
GHeeJeon <47032054+GHeeJeon@users.noreply.github.com>
2024-01-03 03:11:58 +0900

    Update 42586_전지희.java

--- 4주차/42586/42586_전지희.java
+++ 4주차/42586/42586_전지희.java
@@ -6,20 +6,20 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<progresses.length; i++){
+        for(int i = 0; i < progresses.length; i++){
             queue.add((int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
         }
         
         List<Integer> answer = new ArrayList<>();
         while (!queue.isEmpty()){
-            int day = queue.poll();
-            int cnt = 1;
+            int date = queue.poll();
+            int deploy = 1;
             
-            while(!queue.isEmpty() && day >= queue.peek()){
-                cnt++;
+            while(!queue.isEmpty() && date >= queue.peek()){
+                deploy++;
                 queue.poll();
             }
-            answer.add(cnt);
+            answer.add(deploy);
         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
