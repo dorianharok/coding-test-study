a04206fae62f2af1f2776b1686b5326a8a338a61
GHeeJeon <47032054+GHeeJeon@users.noreply.github.com>
2024-01-10 00:28:00 +0900

    Update 12914_전지희.java

--- 5주차/12914/12914_전지희.java
+++ 5주차/12914/12914_전지희.java
@@ -1,13 +1,17 @@
 class Solution {
-    public long solution(int n) {
-        long[] dp = new long[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+      public long solution(int n) {
+          long answer = 0;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
-        }
+              if(n >= 1 && n <= 2000) {
+                  long[] dp = new long[n + 1];
+                  dp[0] = 1;
+                  dp[1] = 2;
+                  for(int i = 2; i < n; i++) {
+                      dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+                  }
+                  answer = dp[n - 1];
+              }
 
-        return dp[n];
-    }
+          return answer;
+      }
 }
